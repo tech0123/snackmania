2adc3127da83aa350e80958d3675af8739ceb14b
hd <classylifestyling@gmail.com>
2024-01-10 15:28:41 +0530

    customtypewitereffect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 import Founder from './Founder';
 import Menu from './Menu';
-import Typewriter from 'typewriter-effect';
+
 
 const Home = () => {
+
+
+
+  const text = 'nacksMania';
+  const [displayText, setDisplayText] = useState('');
+
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const typeText = async () => {
+      while (isMounted) {
+        for (let i = 0; i <= text.length; i++) {
+          if (!isMounted) break;
+
+          setDisplayText(text.slice(0, i));
+          await sleep(240); // Front speed: Adjust the time delay for each character
+
+          if (i === text.length) {
+            await sleep(1000); // Pause after completion of front effect (1,000 milliseconds)
+
+            for (let j = text.length; j >= 0; j--) {
+              if (!isMounted) break;
+
+              setDisplayText(text.slice(0, j));
+              await sleep(150); // Back speed: Adjust the time delay for each character
+            }
+          }
+        }
+      }
+    };
+
+    typeText();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+
 
   const options = {
     initial: {
@@ -24,22 +66,18 @@ const Home = () => {
       <section className='home'>
 
         <div>
-          <motion.h1 {...options}>
-
-            <Typewriter
-
-              options={{
-                strings: "SnacksMania",
-                autoStart: true,
-                loop: true,
-                cursor: "",
-                wrapperClassName: "typewriter",
-                skipAddStyles: true,
-              }}
-            />
 
 
+
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            S{displayText}
           </motion.h1>
+
+
 
           <motion.p
             {...options}
